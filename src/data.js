4d6c4f2a3ab2088f33ffaae0a5ec7aad1c8f89e1
Adrishti <anushkathapliyal2003@gmail.com>
2023-10-01 16:13:01 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const data = [
+	{
+		id: 1,
+		name: "Gopinath Mandir",
+		info: "Gopeshwar is located at a height of 1300 metres above sea level in the Chamoli district of Uttarakhand which is linked with Lord Krishna.Once Lord Shiva was impressed by the tune of the flute of Lord Krishna, who was playing at a maharaas in Vrindavan.",
+		image: "https://www.euttaranchal.com/tourism/timthumb.php?src=https://www.euttaranchal.com/tourism/photos/gopinath-temple-2710382.jpg&w=750&h=510&q=50",
+		price: "8000",
+	},
+	{
+		id: 2,
+		name: "Madhmaheshwer Trek",
+		info: "Madhmaheshwar is mystically covered by the snow-capped Himalayas on the right side and lush green alpine meadows on the left side.The old Huts, village houses,and old temple complete this town.",
+		image: "https://images.prismic.io/indiahike/5cba676d-2c0f-4147-8549-1ae452aca1ba_Madhmaheshwar+-+Mahendra+-+October+-+Indiahikes+-+Contribution+%2837%29+copy.jpg?auto=compress,format",
+		price: "8500",
+	},
+	{
+		id: 3,
+		name: "Joshimath",
+		info: "Narsingh Temple Joshimath has a history of around 1200 years and was home to a sage called Badri Nath. Also, the idol that sits inside was constructed during the reign of Lalitaditya. Lord Vishnu appeared in Narsimha avatar to kill the arrogant demon Hiranyakashyap",
+		image: "https://www.euttaranchal.com/tourism/timthumb.php?src=https://www.euttaranchal.com/tourism/photos/narsingh-temple-8854434.jpg&w=750&h=510&q=50",
+		price: "8000",
+	},
+	{
+		id: 4,
+		name: "Atri muni caves",
+		info: "Atri Muni Fall is a 70 mts waterfall located Near Chopta, Uttarakhand (This fall is near to Chopta but lies in Chamoli District and a trek of 5.5km from Mandal, which is about to 19km from Gopeshwar). It is about to 20Km by road from Chopta station and 5.5Km easy trek up to the Fall.",
+		image: "https://travellingslacker.com/wp-content/uploads/2018/06/Anasuya-Atri-Muni-Trek-27-1200x900-cropped.jpg",
+		price: "7000",
+	},
+	{
+		id: 5,
+		name: "Koteshwer Mahadev",
+		info: "The location is the spot (according to local mythology)where Lord Shiva had stopped for meditation on his way to Kedarnath.The temple has existed since the time of Bhasmasura.",
+		image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Koteshwar1.jpg",
+		price: "3700",
+	},
+	{
+		id: 6,
+		name: "Raghunath ji temple",
+		info: " The temple is originally in existence from the 9th to 10th century. The temple was originally believed to have been established by Adi Shankara during the 8th century, with later expansions by the Garhwal Kingdom.",
+		image: "https://assets.traveltriangle.com/blog/wp-content/uploads/2019/05/Confluence-of-Alaknanda-and-Bagirathi-Rivers1.jpg",
+		price: "5500",
+	}
+	
+];
+
+export default data;
